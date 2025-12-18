0bb37466e1ecbd420028c4dc1e0403f182cfcc97
MarianaBraga13 <maribragad@gmail.com>
2025-12-18 20:35:02 -0300

    tela de abertura

---
+++ game.py
@@ -0,0 +1,13 @@
+WIDTH = 800
+HEIGHT = 600
+
+TITLE = "Python Adventure"
+
+def draw():
+    screen.clear()
+    screen.draw.text(
+        "Python Adventure",
+        center=(WIDTH // 2, HEIGHT // 2),
+        fontsize=48,
+        color="white"
+    )
