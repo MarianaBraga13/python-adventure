6c4c450ccaadaa6dca484def6063f075634c687c
MarianaBraga13 <maribragad@gmail.com>
2025-12-18 23:02:29 -0300

    testando movimentos - herói

--- game.py
+++ game.py
@@ -12,7 +12,7 @@ music_on = True
 #Buttons
 start_button = Rect((300,220), (200, 50))
 music_button = Rect((300,290), (200, 50))
-exit_button = Rect((300,220), (200, 50))
+exit_button = Rect((300,360), (200, 50))
 
 def draw():
     screen.clear()
@@ -45,15 +45,69 @@ def draw_button(rect, text):
         color="white"
     )
 
+#Snake - Hero
 
 def draw_game():
-    screen.draw.text(
-        "GAME RUNNING...",
-        center=(WIDTH // 2, HEIGHT // 2),
-        fontsize=40,
-        color="white"
-    )
+    screen.clear()
+    snake.draw()
 
+class Snake:
+    def __init__(self):
+        self.x = WIDTH // 2
+        self.y = HEIGHT // 2
+
+        self.idle_frames = [
+            "snake/idle_0",
+            "snake/idle_1",
+            "snake/idle_2"
+        ]
+
+        self.move_frames = [
+            "snake/move_0",
+            "snake/move_1",
+            "snake/move_2"
+        ]
+
+        self.frame_index = 0
+        self.animation_timer = 0
+        self.speed = 3
+        self.moving = False
+
+        self.actor = Actor(self.idle_frames[0], (self.x, self.y))
+
+    def update(self):
+        self.moving = False
+
+        if keyboard.left:
+            self.x -= self.speed
+            self.moving = True
+
+        if keyboard.right:
+            self.x += self.speed
+            self.moving = True
+
+        self.actor.pos = (self.x, self.y)
+        self.animate()
+
+    def animate(self):
+        self.animation_timer += 1
+
+        if self.animation_timer >= 10:
+            self.animation_timer = 0
+            self.frame_index = (self.frame_index + 1) % 3
+
+            if self.moving:
+                self.actor.image = self.move_frames[self.frame_index]
+            else:
+                self.actor.image = self.idle_frames[self.frame_index]
+
+    def draw(self):
+        self.actor.draw()
+        snake = Snake() 
+
+    def update():
+        if game_state == GAME:
+            snake.update()          
 
 def on_mouse_down(pos):
     global game_state, music_on
@@ -67,3 +121,5 @@ def on_mouse_down(pos):
 
         elif exit_button.collidepoint(pos):
             quit()
+
+         
